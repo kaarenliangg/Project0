74e4e6ef40984160ba7682c2355de8f885e6c780
kaarenliangg <karenliang9546@gmail.com>
2023-04-04 09:11:12 +1000

    start of day 2

--- js/game.js
+++ js/game.js
@@ -1,35 +1,106 @@
 console.log('sos');
 
-// create an array of the buttons
 
+$(document).ready(function () { 
 
-$(document).ready(function () {
-    let player1 = 'X';
-    let player2 = 'O';
+    // // array of all spots on the board
+    // const buttons = document.querySelectorAll('.grid-item'); // Node List
+    // buttons = Array.from(buttons); // array
 
-    // Create an array of buttons
-    const buttons = document.querySelectorAll('.grid-item');
+    // // empty board 
+    // let board = [
+    //     [ , , ,],
+    //     [ , , ,],
+    //     [ , , ,],
+    // ];
+
+    // start with X
+    let currentPlayer = 'X'
+
+    let numPlays = 0;
+
+    // create empty arrays to push values into so that we can check with the winning function
+    let score = {
+        X: [],
+        O: [],
+    }
+ 
+    // when the player clicks, activate the function
+    $('.grid-item').on('click', function() {
+
+        // start number of plays and ++ (add 1) each turn
+        numPlays++;
+
+        // update the grid with text of currentPlayer
+        $(this).text(currentPlayer);
+        
+        // pushing the index into the score array
+        score[currentPlayer].push(Number($(this).data('index')));
+
+        // before switching to new player, check to see if there is a winner
+
+        if (won()) {
+            alert('Winner: ', $('currentPlayer')) // change to something else later
+        }
+
+        // starts with 'X' and alternates between using if statements
+        if (currentPlayer === 'X') {
+            currentPlayer = 'O';
+        } else {
+            currentPlayer = 'X';
+        }
+          
+    });
+
+   
     
-    // Identify winning solutions in array 
-    [0, 1, 2], // first row
-    [3, 4, 5], // second row
-    [6, 7, 8] // third ropw
+   
+    // if (firstPlayer = player1) {
+    //     board.addEventListener('click', function() {
+    //         board.innerText = player1
+    //     })
+    // }
 
-    // ROUND 1 - 9 spots available
-    $('#1').on('click', function() {
-        $('#1').addClass('x');
+
+    const winning = [
+ 
+        [0, 1, 2], // first row
+        [3, 4, 5], // second row
+        [6, 7, 8] // third row
+        
+        [0, 3, 6], // first column
+        [1, 4, 7], // second column
+        [2, 5, 8],// third column
+    
+        [0, 4, 8], // diagonal #1
+        [2, 4, 6], // diagonal #2
+
+ 
+    ];
+
+    // const validateMove = function() {
+    //     if ($('grid-item') === '') {
+    //         return true;
+    //     } else {
+    //         return false;
+    //     }
+    // }
+
+
+
+    
+    // $('#0').on('click', function() {
+    //     $('#0').html('x');
+
+    // });
+
+    // $('#1').on('click', function() {
+    //     $('#1').html('o');
+
+    // });
+
+    // $('#2').on('click', function() {
+        // $('#2').html('x');
 
     });
 
-
-    // How to know which side is starting the game
-    
-    // start with player 1 default as X then build into selecting their own shape
-
-    // create win for X 
-    // create win for O 
-
-    // make sure players are unable to click the same box twice
-
-    // winner if 3 of the same classes are matching horizontally, vertically, and diagonally.
-});
---
+++ js/games_backup.js
@@ -0,0 +1,47 @@
+// console.log('sos');
+
+// // create an array of the buttons
+
+
+// $(document).ready(function () {
+    
+
+//     $('#0').on('click', function() {
+//         $('#0').addClass('x');
+
+//     });
+
+//     let player1 = 'X'; // addClass 'x'
+//     let player2 = 'O'; // addClass 'o'
+
+//     // Create an array of buttons
+//     const buttons = document.querySelectorAll('.grid-item');
+//     console.log(buttons);
+
+
+//     // Identify winning solutions in array 
+//     [0, 1, 2], // first row
+//     [3, 4, 5], // second row
+//     [6, 7, 8] // third row
+    
+//     [0, 3, 6], // first column
+//     [1, 4, 7], // second column
+//     [2, 5, 8],// third column
+
+//     [0, 4, 8], // diagonal #1
+//     [2, 4, 6], // diagonal #2
+
+//     // ROUND 1 - 9 spots available
+
+
+//     // How to know which side is starting the game
+    
+//     // start with player 1 default as X then build into selecting their own shape
+
+//     // create win for X 
+//     // create win for O 
+
+//     // make sure players are unable to click the same box twice
+
+//     // winner if 3 of the same classes are matching horizontally, vertically, and diagonally.
+// });
